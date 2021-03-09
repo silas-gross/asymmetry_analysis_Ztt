7d901a35829c100485830c0928a6a1ec82e69f8b
Silas Grossberndt <silas.grossberndt@gmail.com>
2021-03-09 12:22:28 -0500

    changed output ofbash script, just going to do asymm calc in c++

--- full_analysis.sh
+++ full_analysis.sh
@@ -1,17 +1,38 @@
+#!/bin/bash
 run_folders=`ls ~/ctG_runs` 
+held_runs=$run_folders
 for run in $run_folders
 do 
 	echo $run
 	./dataSilas.sh $run
 done 
-counter=-0.9
-data_files=`ls ~/ctG_runs/*.dat`
-until $counter==1 
-do 
-	for data in $data_files
-	do 
+counter=-9
+#cd ~/ctG_runs
+#until $counter == 9
+#do 
+#	b2_tt_vals=(-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 1,2,3,4,5,6,7,8,9,10)
+#	b2_lab_vals=(-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 1,2,3,4,5,6,7,8,9,10)
+#	b4_tt_vals=(-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 1,2,3,4,5,6,7,8,9,10)
+#	b4_lab_vals=(-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 1,2,3,4,5,6,7,8,9,10)
+
+
+#	for data in $held_runs 
+#	do
+#		i=0
+#		paste <(awk '{if ($1 < $4/10) sum1+=$2;};{if ($1 > $4/10) sum2+=$2;}; END{print "Ab2 (ttZ) = "(sum2-sum1)/(sum1+sum2);}' b2_$data.dat $counter) >> b2_tt_vals[$i] <(awk '{if ($1 < $4/10) sum1+=$2;};{if ($1 > $4/10) sum2+=$2;}; END{print "Ab4 (ttZ) = "(sum2-sum1)/(sum1+sum2);}' b4_$data.dat) >>b4_tt_vals[$i]
+#		paste <(awk '{if ($1 < $4/10) sum1+=$3;};{if ($1 > $4/10) sum2+=$3;}; END{print "Ab2 (lab) = "(sum2-sum1)/(sum1+sum2);}' b2_$data.dat) <(awk '{if ($1 < $4/10) sum1+=$3;};{if ($1 > $4/10) sum2+=$3;}; END{print "Ab4 (lab) = "(sum2-sum1)/(sum1+sum2);}' b4_$data.dat $counter)>>b4_lab_vals[$i]
+#		((i++)) 
+#	done
+#	echo $b2_tt_vals >> b2_ttZ_frame_asym_at_$counter.txt
+#	echo $b4_tt_vals >> b4_ttZ_frame_asym_at_$counter.txt
+#	echo $b2_lab_vals >> b2_lab_frame_asym_at_$counter.txt
+#	echo $b4_lab_vals >> b4_lab_frame_asym_at_$counter.txt
+
+#	((counter++))
+#done
+#sudo rm ~/ctG_runs/*.dat
 	#implement the asymmetry analysis
 	#need to extract the value for ctG somehow
 	#look for value from param card?
-	
+#forget this, doing this part in the c++ file instead, way easier	
 
--- make_plots.cpp
+++ make_plots.cpp
@@ -1,18 +1,54 @@
-include <fstream>
-include <iostream>
-include <cstdio>
-inclute <cstdlib>
-include "Root.h"
-include "TH1.h"
-include "TFile.h"
-
-void make_single_asym()
+#include <fstream>
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include "TH1.h"
+#include "TFile.h"
+#include <vector>
+#include <sstream>
+#include <utility>
+void calculate_assym()
 {
+	
+}
+
+void make_single_asym(std::ifstream* f, float asymm_pt)
+{
+	float sum_left=0, sum_right=0, sum_left_lab=0, sum_right_lab=0;
+	std::string val;
+	std::vector <float> col1, col2, col3;
+	while (f->is_open() && !f->eof())
+	{
+		while(getline(*f, line, '\t'))
+		{
+			a
+		}
 }
 int main()
 {
-	std::fstream f1=aysmm_data.txt
+	std::vector <std::ifstream> input_b2_data_files, input_b4_data_files;
+	std::vector <std::string> repo_names;
+	std::pair <float, float> ctg_vals_with_cross_section;
+	std::fstream input_file;
+	input_file.open("ctg_repo_names.txt");
+	std::string file_loc="~/ctG_runs/"
+	std::string line;
+	while ( input_file.is_open()==true && ! input_file.eof())
+	{
+		while(getline(input_file, line)) repo_names.push_back(line);
+	}
+	for (int i =0; repo_names.size(), i++)
+	{
+		ifstream f2, f4;
+		f2.open(file_loc+"b2_"+repo_names.at(i)+".dat");
+		f4.open(file_loc+"b4_"+repo_names.at(i)+".dat");
+		input_b2_data_files.push_back(f2);
+		input_b4_data_files.push_back(f4);
+		//this has loaded in the data files
+		//need to grab the ctG value and the cross section here as well
+	}
 	
+
 	return 0;
 
 }
