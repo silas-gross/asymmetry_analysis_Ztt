471808cbb0724fad3dc62314afbcc97528090f8a
Silas Grossberndt <silas.grossberndt@gmail.com>
2021-03-12 01:19:09 -0500

    first pass at the integration of data into the root file. Need to acutally do the graphs, have just loaded the data in at this point

--- make_plots.cpp
+++ make_plots.cpp
@@ -1,54 +1,72 @@
-#include <fstream>
+#include <fsteam>
 #include <iostream>
 #include <cstdio>
 #include <cstdlib>
-#include "TH1.h"
-#include "TFile.h"
 #include <vector>
+#include <string>
 #include <sstream>
 #include <utility>
-void calculate_assym()
-{
-	
-}
+#include "TH1.h"
+#include "TFile.h"
+#include "THistPainter.h"
 
-void make_single_asym(std::ifstream* f, float asymm_pt)
+std::vector < float > get_vals_of_Ab4 (std::fstream* f)
 {
-	float sum_left=0, sum_right=0, sum_left_lab=0, sum_right_lab=0;
-	std::string val;
-	std::vector <float> col1, col2, col3;
-	while (f->is_open() && !f->eof())
-	{
-		while(getline(*f, line, '\t'))
-		{
-			a
-		}
-}
-int main()
-{
-	std::vector <std::ifstream> input_b2_data_files, input_b4_data_files;
-	std::vector <std::string> repo_names;
-	std::pair <float, float> ctg_vals_with_cross_section;
-	std::fstream input_file;
-	input_file.open("ctg_repo_names.txt");
-	std::string file_loc="~/ctG_runs/"
+	//This function will output a pair giving the value of AB4 in lab/ttZ frame for a particluar asymm
+	//the file will be read in as Asym val* 10 \n A b2 (ttZ)=val   A b4 (ttZ)= val \n etc
 	std::string line;
-	while ( input_file.is_open()==true && ! input_file.eof())
+	bool ready_to_read=false;
+	float datapts[3]; //point around which we take the asymm, b4 at ttZ, b4 at lab
+	std::vector < float > data;
+	while(f->is_open() && !f->eof())
 	{
-		while(getline(input_file, line)) repo_names.push_back(line);
+		while(getline(f, line))
+		{
+			std::stringstream part_vals(line), subpart;
+			std::string part, sub;
+			if (getline(part_vals, part, ' '))
+			{
+				if(part.find('=')==std::string::npos) datapts[0]=std::stof(part)/10;
+				if(part.find("b4") != std::string::npos)
+				{
+					if (part.find("ttZ") !==std::string::npos)
+					{
+						subpart(part);
+						if(getline(subpart, sub, '=') && sub.find("(")==std::string::npos) datapts[1]=std::stof(sub);
+					}
+				
+
+					if (part.find("lab") !==std::string::npos)
+					{
+						subpart(part);
+						if(getline(subpart, sub, '=') && sub.find("(")==std::string::npos) datapts[2]=std::stof(sub);
+						ready_to_read=true;
+					}
+				}
+			}
+			if(ready_to_read)
+			{
+				data.push_back(datapts);
+				read_to_read=false;
+			}
+		}
 	}
-	for (int i =0; repo_names.size(), i++)
+	return data;
+}
+void make_graphs ( std::vector < std::pair <float, std::vector <float > > >)
+{
+	shshsdh
+}
+int main(int argc, char** argv)
+{
+	std::fstream input_filenames;
+	input_filenames.open(argv[1]);
+	std::vector < std::fstream > data_files;
+	std::string line;
+	while( input_filename.is_open() && !input_filenames.eof() )
 	{
-		ifstream f2, f4;
-		f2.open(file_loc+"b2_"+repo_names.at(i)+".dat");
-		f4.open(file_loc+"b4_"+repo_names.at(i)+".dat");
-		input_b2_data_files.push_back(f2);
-		input_b4_data_files.push_back(f4);
-		//this has loaded in the data files
-		//need to grab the ctG value and the cross section here as well
+		getline(input_filename, line);
+		data_files.push_back( std::fstream file(line));
 	}
 	
-
-	return 0;
-
 }
