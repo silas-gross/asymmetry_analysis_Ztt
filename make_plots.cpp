4508fb43ccc828193b8c63545a6b9b35a2d537d8
Silas Grossberndt <silas.grossberndt@gmail.com>
2021-03-16 02:25:05 -0400

    script written, just need to decide how to get the ctG value and the xsec right now, maybe change output of the results file??

--- make_plots.cpp
+++ make_plots.cpp
@@ -53,9 +53,63 @@ std::vector < float > get_vals_of_Ab4 (std::fstream* f)
 	}
 	return data;
 }
-void make_graphs ( std::vector < std::pair <float, std::vector <float > > >)
+std::vector < std::pair < std::string, std::vector < float > > > re_arange_data (std::vector < std::pair < float, std::vector <float > > >data)
 {
-	shshsdh
+	std::vector <std::pair < std::string, std::vector<float> > > vals [data.at(0).second().size()]; //needs to go ttZ then lab then xsec, finaly the val of the pit of interest
+	std::vector < std::vector<float >> dummy [data.at(0).second.size()];
+	
+	for (int i =0; i<data.size(), i++)
+	{
+		float ctg=data.at(i).first()[0];
+		float xsec=data.at(i).first()[1]
+		float val[4];
+		for(int j=0; j<data.at(i).second().size(); j++)
+		{
+			val[0]=ctg; 
+			val[1]=xsec;
+			val[2]=data.at(i).second().at(j)[1];
+			val[3]=data.at(i).second().at(j)[2];
+			dummy.at(j).push_back(val);
+		}
+	}
+	for (int i=0; i<dummy.size(); i++)
+	{
+		std::string asympt=std::string::to_string(data.at(0).second().at(i)[0]);
+		std::pair <std::string, std::vector <float > > p;
+		p.first()=asympt;
+		p.second()=dummy.at(i);
+		vals.at(i)= p;
+	}
+	return vals;
+}
+		
+	
+void make_graphs ( std::vector <std::pair<std::string, std::vector<float> > > data)
+{
+	for(int j=0; j<data.size(); j++){
+		std::string namet="b4_tt_Z_"+data.at(j).first(), titlet="Asymmetery on b4 (ttZ) around "+data.at(j).first();
+		std::string namel="b4_lab_"+data.at(j).first(), titlel="Asymmetery on b4 (lab) around "+data.at(j).first();
+		TH1F* b4_ttZ = new TH1F(namet.c_std(),titlet.c_std(), 21, -1, 1);
+		TH1F* b4_lab=new TH1F(namel.c_std(), titlel.c_std(), 21, -1, 1);
+		std::string xs="xsec"+data.at(j).first();
+		TH1F* xsec=new TH1F (xs.c_std(), "Cross Section", 21, -1, 1);
+		float ref_vals[4];
+		for (int i=0; i <data.at(j).second().size(); i++)
+		{
+			if (data.at(j).second().at(i)[0]==0) ref_vals=data.at(j).second().at(i);
+			else continue;
+		}
+		for (int i =0; i<data.at(j).second().size(); i++)
+		{
+			float d = data.at(j).second().at(i);
+			xsec->SetBinContent(i, d[1]/ref_vals[1]);
+			b4_ttZ->SetBinContent(i, d[2]/ref_vals[2]);
+			b4_lab->SetBinContent(i, d[3]/ref_vals[3]);
+		}
+		xsec->Write();
+		b4_ttZ->Write();
+		b4_lab->Write();
+	}	 
 }
 int main(int argc, char** argv)
 {
